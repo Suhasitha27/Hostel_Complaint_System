96922e2ead6252ad93556c81b5052bf797af047d
Suhasitha Reddy <suhasithar@gmail.com>
2025-11-04 11:43:14 +0530

    Updated API base URL to new backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hostel-complaint-system-5sqc.onrender.com/api",
+  baseURL: "https://hostel-complaint-system-1-5ez7.onrender.com/api",
+
 });
 
 API.interceptors.request.use((req) => {
