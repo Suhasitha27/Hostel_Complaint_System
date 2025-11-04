66c340e7c26454b172f0a848491ba72638584068
Suhasitha Reddy <suhasithar@gmail.com>
2025-11-04 10:06:41 +0530

    Update API baseURL for Render backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,9 @@
+// frontend/src/api.js
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: "https://hostel-complaint-system-5sqc.onrender.com/api",
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
